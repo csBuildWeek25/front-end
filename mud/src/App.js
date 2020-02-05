6d9f069d71925dabcc59d83c19c0a8d778eaf3cb
TheHaunted <kevin_lopez@live.com>
2020-02-05 11:37:31 -0600

    creates protected routes for login/register

--- mud/src/App.js
+++ mud/src/App.js
@@ -2,11 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import Register from './components/Register';
+
+const ProtectedRoute = ({component: Component, ...rest}) => {
+  return <Route {...rest} render={props => 
+    localStorage.getItem('token') ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to="/login" />
+    )
+  } />
+}
 
 function App() {
   return (
     <div className="App">
-      <Login className="login" />
+      <Route path="/login" component={Login} />
+      <ProtectedRoute path="/register" component={Register} />
     </div>
   );
 }
