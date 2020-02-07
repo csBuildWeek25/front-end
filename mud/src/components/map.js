c30c4ff8a215b3b22209461d1455856b28559710
ch04937 <97hernandez.c@gmail.com>
2020-02-06 21:05:21 -0800

    displays on map

--- mud/src/components/map.js
+++ mud/src/components/map.js
@@ -10,34 +10,45 @@ const Map = () => {
 	useEffect(() => {
 		async function fetchMapLayout() {
 			console.log("this here");
-			const getDirections = await axios.get(
-				"http://localhost:8000/api/adv/map"
-			);
-			setmap(getDirections);
+			const getMap = await axios.get("http://localhost:8000/api/adv/map");
+			setmap({ getMap });
 		}
 		fetchMapLayout();
 	}, []);
-	console.log(map);
+	console.log(map.getMap);
 	return (
-		<div className="map">
-			<div className="content">
-				<div>
+		<div className="game">
+			<div className="map">
+				<div className="content">
 					<Card.Group>
-						{/* {map.data &&
-							map.data.map(data => (
-								<div key={data.id}>
-									<Card.Header>{data.title}</Card.Header>
-									<Card.Description>
-										{data.description}
-									</Card.Description>
-									<Button>North</Button>
-									<Button>South</Button>
-									<Button>East</Button>
-									<Button>West</Button>
+						{map.getMap &&
+							map.getMap.data.map.map(data => (
+								<div
+									key={data.id}
+									style={{
+										display: "flex",
+										flexWrap: "wrap",
+										height: "100px",
+										width: "100px",
+										margin: "30px 30px",
+									}}
+								>
+									<Card>
+										<Card.Header>{data.title}</Card.Header>
+										<Card.Description>
+											{data.description}
+										</Card.Description>
+									</Card>
 								</div>
-							))} */}
+							))}
 					</Card.Group>
 				</div>
+			</div>
+			<div className="buttons">
+				<Button className="btn">North</Button>
+				<Button className="btn">South</Button>
+				<Button className="btn">East</Button>
+				<Button className="btn">West</Button>
 			</div>
 		</div>
 	);
